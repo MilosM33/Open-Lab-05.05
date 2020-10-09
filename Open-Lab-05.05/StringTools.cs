1d2b5634b67a6fe2bcda0ce3294c4037d298c9f3
MilosM33 <milosmarejka89@gmail.com>
2020-10-09 22:15:34 +0200

    init

--- Open-Lab-05.05/StringTools.cs
+++ Open-Lab-05.05/StringTools.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_05._05
 {
     public class StringTools
     {
+        
         public string AlternatingCaps(string original)
         {
-            throw new NotImplementedException();
+            string temp = "";
+            
+            bool upper = false;
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (char.IsLetter(original[i]))
+                {
+                    if (upper)
+                        temp += char.ToLower(original[i]);
+                    else
+                        temp += char.ToUpper(original[i]);
+
+                    upper = !upper;
+
+                }
+                else
+                    temp += original[i];
+                
+            }
+            return temp;
         }
+
+
     }
 }
